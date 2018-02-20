555298c5a24da1684de78f0b7ade5550581c1cce
Allen Gu <gu.a@mac2687.wehi.edu.au>
2018-02-20 19:56:01 +1100

    Contour experiments

---
+++ binaryBlobTest/saveIndiv.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+
+for i in range(10):
+	img = cv2.imread('binaryBlob.png')
+	imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	ret, thresh = cv2.threshold(imgGrey, 127, 255, 0)
+	img2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+	cv2.drawContours(img, contours, i, (0,255,0), 3)
+	cv2.imshow("LMAO_%s"%str(i), img)
+cv2.waitKey(10000)
---
+++ blobTest/blob.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+# Read image
+im = cv2.imread("blob.jpg", cv2.IMREAD_GRAYSCALE)
+
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+# Change thresholds
+params.minThreshold = 10
+params.maxThreshold = 200
+
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 10
+
+# Filter by Circularity
+params.filterByCircularity = False
+params.minCircularity = 0.1
+
+# Filter by Convexity
+params.filterByConvexity = True
+params.minConvexity = 0.98
+    
+# Filter by Inertia
+params.filterByInertia = False
+params.minInertiaRatio = 0.01
+
+# Create a detector with the parameters
+detector = cv2.SimpleBlobDetector_create(params)
+
+
+# Detect blobs.
+keypoints = detector.detect(im)
+
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
+# the size of the circle corresponds to the size of blob
+
+im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# Show blobs
+cv2.namedWindow('ImageWindowName', cv2.WINDOW_NORMAL)
+cv2.imshow('ImageWindowName',im_with_keypoints)
+cv2.waitKey(0)
