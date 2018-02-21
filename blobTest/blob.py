e95d03392bf498da738f36e5bdc67ceebc3c8af7
AliGeeee <gua@student.unimelb.edu.au>
2018-02-22 09:25:06 +1100

    worked at home for a bit

--- blobTest/blob.py
+++ blobTest/blob.py
@@ -2,19 +2,18 @@ import cv2
 import numpy as np
 
 # Function for generating parameters
-def modParams(thresh = (None,None),area = (None,None),circ = (None,None),conv = (None,None),iner = (None,None)):
+def modParams(area = (None,None),circ = (None,None),conv = (None,None),iner = (None,None)):
 	#initiate params
 	params = cv2.SimpleBlobDetector_Params()
 
 	#setting up filters
-	params.filterByColor = False if thresh==(None,None) else True
 	params.filterByArea = False if area==(None,None) else True
 	params.filterByCircularity = False if circ==(None,None) else True
 	params.filterByConvexity = False if conv==(None,None) else True
 	params.filterByInertia = False if iner==(None,None) else True
 
 	#setting min and max params
-	(params.minThreshold, params.maxThreshold) = thresh
+	(params.minThreshold, params.maxThreshold) = (127,255)
 	(params.minArea, params.maxArea) = area
 	(params.minCircularity, params.maxCircularity) = circ
 	(params.minConvexity, params.maxConvexity) = conv
@@ -28,28 +27,28 @@ def modParams(thresh = (None,None),area = (None,None),circ = (None,None),conv = 
 # 				   conv = (0.2, None),
 # 				   iner = (None, None))
 
-params = modParams(thresh=(None, None),
-				   area = (None, None),
-				   circ = (None, None),
-				   conv = (None, None),
-				   iner = (None, None))
+params = modParams(area = (1, None),
+				   circ = (0, 1),
+				   conv = (0, 1),
+				   iner = (0, 1))
 
 # Read image
-im = cv2.imread("blob.jpg", cv2.IMREAD_GRAYSCALE)
+im = cv2.imread("blob.png", cv2.IMREAD_GRAYSCALE)
+ret, thresh = cv2.threshold(im, 127, 254, cv2.THRESH_BINARY_INV)
 
 # Create a detector with the parameters
 detector = cv2.SimpleBlobDetector_create(params)
 
 # Detect blobs.
-keypoints = detector.detect(im)
+keypoints = detector.detect(thresh)
 for i in keypoints:
 	print(i.pt, i.size)
-
+print(len(keypoints))
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
 # the size of the circle corresponds to the size of blob
 
-im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+im_with_keypoints = cv2.drawKeypoints(thresh, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 # Show blobs
 cv2.namedWindow('ImageWindowName', cv2.WINDOW_NORMAL)
