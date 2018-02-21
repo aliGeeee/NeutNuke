bab319f294171211ca401d62ff455ccc83706c71
Allen Gu <gu.a@mac2687.wehi.edu.au>
2018-02-21 19:56:57 +1100

    added some stuff

--- blobTest/blob.py
+++ blobTest/blob.py
@@ -1,39 +1,49 @@
 import cv2
 import numpy as np
 
+# Function for generating parameters
+def modParams(thresh = (None,None),area = (None,None),circ = (None,None),conv = (None,None),iner = (None,None)):
+	#initiate params
+	params = cv2.SimpleBlobDetector_Params()
+
+	#setting up filters
+	params.filterByColor = False if thresh==(None,None) else True
+	params.filterByArea = False if area==(None,None) else True
+	params.filterByCircularity = False if circ==(None,None) else True
+	params.filterByConvexity = False if conv==(None,None) else True
+	params.filterByInertia = False if iner==(None,None) else True
+
+	#setting min and max params
+	(params.minThreshold, params.maxThreshold) = thresh
+	(params.minArea, params.maxArea) = area
+	(params.minCircularity, params.maxCircularity) = circ
+	(params.minConvexity, params.maxConvexity) = conv
+	(params.minInertiaRatio, params.maxInertiaRatio) = iner
+
+	return params
+
+# params = modParams(thresh=(10, 200),
+# 				   area = (10,None),
+# 				   circ = (0.5, 1),
+# 				   conv = (0.2, None),
+# 				   iner = (None, None))
+
+params = modParams(thresh=(None, None),
+				   area = (None, None),
+				   circ = (None, None),
+				   conv = (None, None),
+				   iner = (None, None))
+
 # Read image
 im = cv2.imread("blob.jpg", cv2.IMREAD_GRAYSCALE)
-
-# Setup SimpleBlobDetector parameters.
-params = cv2.SimpleBlobDetector_Params()
-
-# Change thresholds
-params.minThreshold = 10
-params.maxThreshold = 200
-
-
-# Filter by Area.
-params.filterByArea = True
-params.minArea = 10
-
-# Filter by Circularity
-params.filterByCircularity = False
-params.minCircularity = 0.1
-
-# Filter by Convexity
-params.filterByConvexity = True
-params.minConvexity = 0.98
-    
-# Filter by Inertia
-params.filterByInertia = False
-params.minInertiaRatio = 0.01
 
 # Create a detector with the parameters
 detector = cv2.SimpleBlobDetector_create(params)
 
-
 # Detect blobs.
 keypoints = detector.detect(im)
+for i in keypoints:
+	print(i.pt, i.size)
 
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
