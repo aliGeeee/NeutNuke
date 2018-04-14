b846fb646d2c87437636caea2ffa52cc473bd1c0
Allen Gu <gu.a@mac2687.wehi.edu.au>
2018-04-14 18:30:52 +1000

    stats csv

--- Pipeline/mainScript.py
+++ Pipeline/mainScript.py
@@ -1,60 +1,59 @@
 import numpy as np
+import pandas as pd
 import cv2
 from matplotlib import pyplot as plt
 import os
 import copy
 
-def removeEdgeObj(x, y, width, height, dimX, dimY):
-	if x == 0 or y == 0:
-		return False
-	elif x+width >= dimX or y+height >= dimY:
-		return False
-	else:
-		return True
-
+#creating an output directory
 try:
-	os.mkdir("output")
+	os.mkdir('output')
 except:
 	pass
 
+#setting manual threshold values
+cellThreshValue = 200
+nucThreshValue = 100
+
+#importing image and making grayscale
 img = cv2.imread('rawCells.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-ret, threshNuclear = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY_INV)
-cv2.imwrite('output/thresh_120.jpg', threshNuclear)
-
-ret, threshCell = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
+#thresholding for cells
+ret, threshCell = cv2.threshold(gray, cellThreshValue, 255, cv2.THRESH_BINARY_INV)
 cv2.imwrite('output/thresh_200.jpg', threshCell)
 
-img2, contours, hierarchy = cv2.findContours(threshCell, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#generating contours around cells
+img2, cellCon, hierarchy = cv2.findContours(threshCell, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 origImg = img
 
+#generate output and summary stats
+nucData = []
+
 #iterate through contours and isolate
-for i in range(len(contours)):
-	area = cv2.contourArea(contours[i])
-	x, y, width, height = cv2.boundingRect(contours[i])
+for i in range(len(cellCon)):
+	area = cv2.contourArea(cellCon[i])
+	x, y, width, height = cv2.boundingRect(cellCon[i])
+
+	#excluding tiny and big blobs
 	if area > 300 and area < 1000:
-		if removeEdgeObj(x, y, width, height, img.shape[1], img.shape[0]) == True:
+		#excluding cells on edges
+		if not (x == 0 or x+width >= img.shape[1] or y == 0 or y+height >= img.shape[0]):
+			#creating mask for cells
 			rawMask = np.zeros((img.shape[0], img.shape[1], 1), dtype = "uint8")
-			cv2.drawContours(rawMask, contours, i, (255), -1)
-
+			cv2.drawContours(rawMask, cellCon, i, (255), -1)
 			revMask = np.bitwise_not(rawMask)
-			#cv2.drawContours(origImg, contours, i, (0,0,255), -1)
-			#cv2.imwrite("Masks_%s.png"%str(i), rawMask)
-
 			res1 = cv2.bitwise_and(img, img, mask = rawMask)
 			res2 = res1 + revMask
-			cv2.imwrite("output/sad.png", res2)
 
+			#isolating cells
 			margin = 100
 			rawROI = np.zeros((height+2*margin, width+2*margin, 3))+255
 			rawROI[margin:margin+height, margin:margin+width] = res2[y:y+height, x:x+width]
 
-			area = cv2.contourArea(contours[i])
-
+			#creating output image of isolated cell
 			imgTxt = copy.copy(rawROI)
-
 			cv2.putText(imgTxt, "Area = %s"%area, (int(0.1*margin),height+int(1.9*margin)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255))
 
 			name = "ROI_%s"%str(i)
@@ -69,29 +68,32 @@ for i in range(len(contours)):
 			except:
 				pass
 
-			cv2.imwrite("output/cells/%s.jpg"%name, imgTxt)
+			cv2.imwrite("output/cells/%s/%s.jpg"%(name,name), imgTxt)
 
 			#nuclear analysis
 			rawROI = rawROI.astype(np.uint8)
 			gray = cv2.cvtColor(rawROI, cv2.COLOR_BGR2GRAY)
 
-			ret, nucThresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY_INV)
+			ret, nucThresh = cv2.threshold(gray, nucThreshValue, 255, cv2.THRESH_BINARY_INV)
 			cv2.imwrite('output/cells/%s/thresh_120.jpg'%name, nucThresh)
 
-			lmao, nucCon, hierarchy = cv2.findContours(nucThresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+			ret, nucConRaw, hierarchy = cv2.findContours(nucThresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+			nucCon = sorted(nucConRaw, key=lambda x: -cv2.contourArea(x))
 
-			n=0
+			n = 1
 			for j in range(len(nucCon)):
-				if hierarchy[0][j][3] == -1:
-					if cv2.contourArea(nucCon[j]) > 3:
-						print(name, cv2.contourArea(nucCon[j]), area, round(cv2.contourArea(nucCon[j])/area,2))
-						n+=1
+				#picking only nuclei and removing tiny artefacts 
+				if hierarchy[0][j][3] == -1 and cv2.contourArea(nucCon[j]) > 3:
+					conArea = cv2.contourArea(nucCon[j])
+					conPeri = cv2.arcLength(nucCon[j], True)
+					conHullArea = cv2.contourArea(cv2.convexHull(nucCon[j]))
+					conCirc = 4*np.pi*conArea/conPeri**2
+					convex = conArea/conHullArea
+					nucData.append([name, n, round(conArea/area, 3), round(conCirc, 3), round(convex, 3)])
+					n+=1
 
-			imgTxt = copy.copy(rawROI)
-
-			cv2.putText(imgTxt, "Area = %s"%area, (int(0.1*margin),height+int(1.9*margin)), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255))
-
-
+			if n>1:
+				print(name)
 			# # noise removal
 			# kernel = np.ones((1,1), np.uint8)
 			# opening = cv2.morphologyEx(threshNuclear, cv2.MORPH_OPEN, kernel, iterations = 2)
@@ -129,5 +131,8 @@ for i in range(len(contours)):
 			# cv2.imwrite('output/cells/%s/markers.jpg'%name,markers)
 			# #cv2.imwrite('output/cells/%s/markersC.jpg'%name,markers2)
 			# cv2.imwrite('output/cells/%s/imggg.jpg'%name,rawROI)
+
+nucDF = pd.DataFrame(np.array(nucData), columns = ["Cell", "Nucleus", "Area Fraction", "Circularity", "Convexity"])
+nucDF.to_csv("nucStats.csv")
 
 print("Done")
