7ca883d9f0df4bc655dc01a260dc3d5f5d5ae643
Allen Gu <gu.a@mac2687.wehi.edu.au>
2018-04-17 17:21:56 +1000

    added more summary statistics

--- Main/mainScript.py
+++ Main/mainScript.py
@@ -16,7 +16,7 @@ except:
 
 #setting manual threshold values
 cellThreshValue = 200
-nucThreshValue = 100
+nucThreshValue = 110
 
 try:
 	dirs = os.listdir('images')
@@ -24,8 +24,13 @@ except:
 	print("Make sure there is folder called 'images' containing input images.")
 	quit()
 
+imageData = []
+
 #importing image and making grayscale
 for imageFile in dirs:
+	if imageFile == ".DS_Store":
+		continue
+
 	print("Analysing %s..."%imageFile)
 	outputDir = 'output/%s'%imageFile[:-4]
 	try:
@@ -33,31 +38,40 @@ for imageFile in dirs:
 	except:
 		pass
 
-	img = cv2.imread('images/%s'%imageFile)
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+	try:
+		img = cv2.imread('images/%s'%imageFile)
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	except:
+		print("%s isn't an image file!"%imageFile)
+		continue
+	
 	#thresholding for cells
-	ret, threshCell = cv2.threshold(gray, cellThreshValue, 255, cv2.THRESH_BINARY_INV)
-	cv2.imwrite('%s/thresh_200.jpg'%outputDir, threshCell)
+	ret, cellThresh = cv2.threshold(gray, cellThreshValue, 255, cv2.THRESH_BINARY_INV)
+	cv2.imwrite('%s/thresh_200.jpg'%outputDir, cellThresh)
 
 	#generating contours around cells
-	img2, cellCon, hierarchy = cv2.findContours(threshCell, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	img2, cellCon, hierarchy = cv2.findContours(cellThresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 	origImg = img
 
 	#generate output and summary stats
 	nucData = []
 	cellData = []
+	totCellArea = 0
+	totNucArea = 0
+	validCells = 0
 
 	#iterate through contours and isolate
 	for i in range(len(cellCon)):
 		cellArea = cv2.contourArea(cellCon[i])
+		totCellArea += cellArea
 		x, y, width, height = cv2.boundingRect(cellCon[i])
 
 		#excluding tiny and big blobs
 		if cellArea > 300 and cellArea < 1000 and calcCirc(cellCon[i])>0.5:
 			#excluding cells on edges
 			if not (x == 0 or x+width >= img.shape[1] or y == 0 or y+height >= img.shape[0]):
+				validCells += 1
 				name = "ROI_%s"%str(i)
 
 				#creating mask for cells
@@ -99,12 +113,12 @@ for imageFile in dirs:
 				nucCon = sorted(nucConRaw, key=lambda x: -cv2.contourArea(x))
 
 				n = 0
-				nucAreaTot = 0
+				locTotNucArea = 0
 				for j in range(len(nucCon)):
 					#picking only nuclei and removing tiny artefacts 
 					if hierarchy[0][j][3] == -1 and cv2.contourArea(nucCon[j]) > 3:
 						conArea = cv2.contourArea(nucCon[j])
-						nucAreaTot += conArea
+						locTotNucArea += conArea
 						conHullArea = cv2.contourArea(cv2.convexHull(nucCon[j]))
 						conCirc = calcCirc(nucCon[j])
 						convex = conArea/conHullArea
@@ -113,7 +127,8 @@ for imageFile in dirs:
 						n+=1
 
 				if n>0:
-					cellData.append([name, cellArea, round(nucAreaTot/cellArea, 3), n])
+					cellData.append([name, cellArea, round(locTotNucArea/cellArea, 3), n])
+					totNucArea += locTotNucArea
 
 	#saving stats as CSVs
 	nucDF = pd.DataFrame(np.array(nucData), columns = ["Cell", "Nucleus", "Area Fraction", "Circularity", "Convexity"])
@@ -122,13 +137,31 @@ for imageFile in dirs:
 	cellDF = pd.DataFrame(np.array(cellData), columns = ["Cell", "Cell Area", "Nuc Fraction", "No. Nuclear Frags"])
 	cellDF.to_csv('%s/cellStats.csv'%outputDir)
 
+	#generating some summary stats
+	nucCircMean = round(np.mean(nucDF['Circularity'].values.astype('float')[nucDF['Area Fraction'].values.astype('float') > 0.1]), 3)
+	nucCircSD = round(np.std(nucDF['Circularity'].values.astype('float')[nucDF['Area Fraction'].values.astype('float') > 0.1]), 3)
+
+	nucConvMean = round(np.mean(nucDF['Convexity'].values.astype('float')[nucDF['Area Fraction'].values.astype('float') > 0.1]), 3)
+	nucConvSD = round(np.std(nucDF['Convexity'].values.astype('float')[nucDF['Area Fraction'].values.astype('float') > 0.1]), 3)
+
+	#saving whole image data
+	imageData.append([imageFile, len(cellCon), validCells, round(totNucArea/totCellArea, 3), nucCircMean, nucCircSD, nucConvMean, nucConvSD])
+
 	#plotting some scatterplots
-	fig = plt.figure()
-	plt.scatter(nucDF['Circularity'].values, nucDF['Area Fraction'].values)
-	plt.title('Test Scatter ')
-	plt.xlabel('Circularity')
-	plt.ylabel('Area Fraction')
-	fig.savefig('%s/testScatter.jpg'%outputDir)
+	xQuant = 'Convexity'
+	yQuant = 'Area Fraction'
+	plt.xlim([0.0, 1.0])
+	plt.ylim([0.0, 1.0])
+	plt.xticks(np.arange(0.0, 1.1, 0.1))
+	plt.yticks(np.arange(0.0, 1.1, 0.1))
+	plt.scatter(nucDF[xQuant].values.astype('float'), nucDF[yQuant].values.astype('float'))
+	plt.title(imageFile)
+	plt.xlabel(xQuant)
+	plt.ylabel(yQuant)
+	plt.savefig('%s/testScatter.jpg'%outputDir)
 	plt.close()
+
+imgDF = pd.DataFrame(np.array(imageData), columns = ["Image File", "No. Cells", "No. Valid Cells", "Nuc. Area Frac.", "Circ. Mean", "Circ. SD", "Conv. Mean", "Conv. SD"])
+imgDF.to_csv('output/imageStats.csv')
 
 print("Done")
