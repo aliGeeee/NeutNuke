e2b05c5fd15695624789306a72f774d734aaf944
Allen Gu <gu.a@mac2687.wehi.edu.au>
2018-04-29 19:02:17 +1000

    just checking that this is up to date

--- Main/doxSum.py
+++ Main/doxSum.py
@@ -26,19 +26,19 @@ xListUnt = ["n=%s"%i for i in untDF["No. Valid Cells"].values.astype('int')]
 for i in range(len(xListUnt)):
 	xListUnt[i] = "%s\n%s"%(i+1, xListUnt[i])
 yListUnt = untDF['%s Mean'%quant].values.astype('float')
-#yErrUnt = untDF['%s SD'%quant].values.astype('float')
+yErrUnt = untDF['%s SD'%quant].values.astype('float')
 
 plt.scatter(xListUnt, yListUnt, color = 'blue', marker = 's', label = 'Untreated')
-#plt.errorbar(xListUnt, yListUnt, yerr = yErrUnt, color = 'blue', linestyle = "None")
+plt.errorbar(xListUnt, yListUnt, yerr = yErrUnt, color = 'blue', linestyle = "None")
 
 xListDox = ["n=%s"%i for i in doxDF["No. Valid Cells"].values.astype('int')]
 for i in range(len(xListDox)):
 	xListDox[i] = "%s\n%s"%(i+1+len(xListUnt),xListDox[i])
 yListDox = doxDF['%s Mean'%quant].values.astype('float')
-#yErrDox = doxDF['%s SD'%quant].values.astype('float')
+yErrDox = doxDF['%s SD'%quant].values.astype('float')
 
 plt.scatter(xListDox, yListDox, color = 'red', marker = 'o', label = 'Dox')
-#plt.errorbar(xListDox, yListDox, yerr = yErrDox, color = 'red', linestyle = "None")
+plt.errorbar(xListDox, yListDox, yerr = yErrDox, color = 'red', linestyle = "None")
 
 plt.legend(loc='upper left')
 plt.savefig('testStatScatter.jpg')
